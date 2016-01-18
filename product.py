af07dca5666aa09d63816481704e69d379f8aefc
jmartin <jmartin@zikzakmedia.com>
2016-01-18 14:00:36 +0100

    Remove name field from product.product model

--- product.py
+++ product.py
@@ -2,26 +2,15 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval
 
 
 __all__ = ['Product']
 __metaclass__ = PoolMeta
-STATES = {
-    'readonly': ~Eval('active', True),
-    'required': ~(Eval('variant_unique_name', False) | Eval(
-        '_parent_template', {}).get('unique_variant', False)),
-    }
 
 
 class Product:
     __name__ = 'product.product'
-    name = fields.Function(
-            fields.Char('Name', states=STATES, depends=['active'], translate=True),
-        'get_name', setter='set_name', searcher='search_name', )
     variant_name = fields.Char("Variant Name", select=True, translate=True)
-    variant_unique_name = fields.Function(fields.Boolean('Variant Unique Name'),
-        'on_change_with_variant_unique_name')
 
     @classmethod
     def search(cls, domain, offset=0, limit=None, order=None, count=False,
@@ -40,22 +29,3 @@ class Product:
             res,
             [('variant_name', ) + tuple(clause[1:])]
             ]
-
-    def get_name(self, name):
-        if self.variant_name:
-            return self.variant_name
-        return self.template.name
-
-    @classmethod
-    def set_name(cls, products, name, value):
-        cls.write(products, {'variant_name': value})
-
-    @classmethod
-    def search_name(cls, name, clause):
-        return [('variant_name',) + tuple(clause[1:])]
-
-    @fields.depends('template')
-    def on_change_with_variant_unique_name(self, name=None):
-        if self.template:
-            return self.template.unique_variant
-        return False
